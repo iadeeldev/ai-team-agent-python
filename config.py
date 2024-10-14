0f2150f1736afd3224610e31fabe2f8b378c069c
Adeel <adeel.bashir@arhamsoft.com>
2024-10-14 15:01:17 +0000

    updated config file

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
-    APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "")
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    # APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
+    # APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "")
